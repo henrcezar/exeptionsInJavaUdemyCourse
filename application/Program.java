a2aa03734aada8815d8e5c0d68cd9990d9a89546
Henrique Cézar Ferreira de Oliveira <henrcezar@gmail.com>
2024-11-20 14:14:35 -0300

    Complemento da estrutura - versão muito ruim de solução do exercício proposto

--- application/Program.java
+++
@@ -1,7 +0,0 @@
-package exceções.exceçõesPersonalizadas.exeptionsInJavaUdemyCourse.application;
-
-public class Program {
-    public static void main(String[] args){
-
-    }
-}
---
+++ metodoMuitoRuim/application/Program.java
@@ -0,0 +1,7 @@
+package exceções.exceçõesPersonalizadas.exeptionsInJavaUdemyCourse.metodoMuitoRuim.application;
+
+public class Program {
+    public static void main(String[] args){
+
+    }
+}
